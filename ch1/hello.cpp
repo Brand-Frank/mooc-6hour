a1d1e8efa040f383a8725cdb7b1782758da97400
Brand-Frank <3069584010@qq.com>
2022-03-21 22:16:59 +0800

    add codes

---
+++ ch1/hello.cpp
@@ -0,0 +1,25 @@
+/*
+ * @Author: codebug 
+ * @Date: 2022-03-21 20:41:39 
+ * @Last Modified by: codebug
+ * @Last Modified time: 2022-03-21 20:42:46
+ */
+#include <iostream>
+
+int fun(int a, int b);
+
+int main(void){
+    int a = 10, b = 2, c;
+    std::cout << "a*13 = " << a * 13 << std::endl;
+
+    c = fun(a,b);
+    std::cout << "c = " << a*b << std::endl;
+    
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
+
+int fun(int a, int b){
+    return a*b;
+}
